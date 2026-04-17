894b522254d2352b9c130848bccc20a8952ae8be
a.armari <a.armari@we-com.it>
2026-04-17 11:54:50 +0200

    security base config

---
+++ src/main/java/it/wecom/fantawecom/configuration/SecurityConfig.java
@@ -0,0 +1,22 @@
+package it.wecom.fantawecom.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
